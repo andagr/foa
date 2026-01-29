c1d91a5f5decd2e16bb55daeb94c68ade37e9170
Andreas Ågren <bandagr@gmail.com>
2026-01-30 00:11:37 +0100

    Fix http publish

--- src/game_client/mygame/app/main.rb
+++ src/game_client/mygame/app/main.rb
@@ -1,8 +1,16 @@
-def greet friend
-  "Hello #{friend}!"
-end
-
 def tick args
-  args.outputs.labels << [120, 120, greet("Dragon")]
-  args.outputs.labels << [120, 100, greet("Andreas")]
+  if args.tick_count == 0
+    # On the web, 'args.gtk.platform' usually returns 'Web' or similar
+    puts "Running on: #{args.gtk.platform}"
+    
+    # This check specifically looks for the JS environment
+    if args.gtk.platform == "Web" # Or sometimes check for 'Emscripten'
+      puts "WebSockets are available via the Browser!"
+    else
+      puts "Running on Desktop: WebSockets disabled in Standard Tier."
+    end
+  end
+
+  args.outputs.labels << [120, 120, "Platform: #{args.gtk.platform}"]
+  args.outputs.labels << [120, 100, "Go to http://localhost:9001 in your browser!"]
 end
